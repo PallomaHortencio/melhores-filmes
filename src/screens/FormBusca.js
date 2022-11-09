8e692f11c96aba5586addb9b1337c4bbf3f319eb
Palloma Hortencio <palloma.hortencio@gmail.com>
2022-11-09 11:06:04 -0300

    Exercicio

--- src/screens/FormBusca.js
+++ src/screens/FormBusca.js
@@ -1,12 +1,68 @@
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TextInput,
+  Button,
+  Alert,
+} from "react-native";
 import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const FormBusca = () => {
+  const [text, onChangeText] = React.useState("");
   return (
     <SafeAreaView>
-      <Text>FormBusca</Text>
+      <Text style={estilos.texto}>
+        Pegue sua pipoca e faça uma seção cinema em casa
+        <MaterialCommunityIcons name="popcorn" size={16} color="black" />
+      </Text>
+      <Text style={estilos.texto}>
+        Localize um filme que você gostaria de ver
+      </Text>
+      <View style={estilos.filme}>
+        <Ionicons name="film" size={44} color="black" />
+        <TextInput
+          style={estilos.caixa}
+          onChangeText={onChangeText}
+          value={text}
+          placeholder="Filmes"
+        />
+      </View>
+      <Button
+        style={estilos.botao}
+        title="Procurar"
+        color="#5451a6"
+        onPress={() => Alert.alert("oi")}
+      />
     </SafeAreaView>
   );
 };
 
 export default FormBusca;
+
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  caixa: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: "90%",
+  },
+  texto: {
+    marginVertical: 6,
+    margin: 5,
+    paddingVertical: 8,
+  },
+  filme: {
+    flexDirection: "row",
+    alignItems: "center",
+    margin: 6,
+  },
+});
