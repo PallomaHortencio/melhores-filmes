252f76fe74b4529ee42af317380e598201168f99
Palloma Hortencio <palloma.hortencio@gmail.com>
2022-11-10 11:55:57 -0300

    Usando Api de filmes

--- src/screens/Resultados.js
+++ src/screens/Resultados.js
@@ -1,14 +1,44 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import api from "../services/api";
 
 const Resultados = ({ route }) => {
   /* Usamos a prop route (do react navigation) para acessar os parametros desta rota de navegação e extrair os dados (neste caso, filme) enviados para essa tela Resultados */
   const { filme } = route.params;
 
+  const [resultados, setResultados] = useState([]);
+
+  useEffect(() => {
+    async function buscarFilmes() {
+      try {
+        const resposta = await api.get("/search/movie", {
+          params: {
+            api_key: "e3e8b219c58ff10013641a39840e71fa",
+            language: "pt-BR",
+            query: filme,
+            include_adult: false,
+          },
+        });
+
+        setResultados(resposta.data.results);
+      } catch (error) {
+        console.log("Deu ruim na busca da API: " + error.message);
+      }
+    }
+    buscarFilmes();
+  }, []);
+
+  console.log(resultados);
+
   return (
     <SafeAreaView style={estilos.container}>
       <Text>Você buscou por: {filme}</Text>
+      <View style={estilos.viewFilmes}>
+        {resultados.map((resultado) => {
+          return <Text key={resultado.id}>{resultado.title}</Text>;
+        })}
+      </View>
     </SafeAreaView>
   );
 };
@@ -19,5 +49,8 @@ const estilos = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
+  },
+  viewFilmes: {
+    marginVertical: 8,
   },
 });
