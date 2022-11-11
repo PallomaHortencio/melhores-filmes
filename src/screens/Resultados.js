866d52fd7bcfd8fdf7c4e3ba97fe48b4d3be09ce
Palloma Hortencio <palloma.hortencio@gmail.com>
2022-11-11 09:59:11 -0300

    Loading

--- src/screens/Resultados.js
+++ src/screens/Resultados.js
@@ -3,19 +3,17 @@ import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import api from "../services/api";
 import apiKey from "../../apiKey";
+import Loading from "../components/Loading";
 
 const Resultados = ({ route }) => {
-  /* Usamos a prop route (do react navigation) para acessar os parametros desta rota de navegação e extrair os dados (neste caso, filme) enviados para essa tela Resultados */
   const { filme } = route.params;
 
   const [resultados, setResultados] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  /* useEffect: hook do React que executa operações no momento em que o componente (neste caso, Resultado) é renderizado */
   useEffect(() => {
-    /* Assim que entrarmos em Resultado, é executado a função async buscarFilmes que por sua vez através do axios executa a consulta á API baseada no filme que foi digitado */
     async function buscarFilmes() {
       try {
-        /* Aguardamos a resposta da consulta get ao endpoint "/search/movie" da api. Observe que este endpoint precisa de parâmetros para a execução correta da consulta. Estes parâmetros DEVEM ter o mesmo nome indicado na documentação do endpoint/API. */
         const resposta = await api.get("/search/movie", {
           params: {
             api_key: apiKey,
@@ -26,6 +24,11 @@ const Resultados = ({ route }) => {
         });
 
         setResultados(resposta.data.results);
+
+        /* Simulando um tempo de carregamento lento usando temporizador setInterval */
+        setInterval(() => {
+          setLoading(false);
+        }, 3000);
       } catch (error) {
         console.log("Deu ruim na busca da API: " + error.message);
       }
@@ -38,10 +41,16 @@ const Resultados = ({ route }) => {
   return (
     <SafeAreaView style={estilos.container}>
       <Text>Você buscou por: {filme}</Text>
+
+      {/* Sintaxe de if evaluate usando && Se loading for TRUE, renderize <Loading /> */}
+      {loading && <Loading />}
+
       <View style={estilos.viewFilmes}>
-        {resultados.map((resultado) => {
-          return <Text key={resultado.id}>{resultado.title}</Text>;
-        })}
+        {/* Se loading for FALSE, renderize o resultado do map */}
+        {!loading &&
+          resultados.map((resultado) => {
+            return <Text key={resultado.id}>{resultado.title}</Text>;
+          })}
       </View>
     </SafeAreaView>
   );
